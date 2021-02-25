009db51c4e3b834f72564ac74fe5306974bb3e03
CarolSi <a.carolg1@gmail.com>
2021-02-25 00:25:16 -0300

    build

---
+++ build/precache-manifest.15e5ee8597d20e9c0f203d337e6a7eec.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbadad8a4a0cf81bc9485f3bf2021990",
+    "url": "/index.html"
+  },
+  {
+    "revision": "914ee45da1d7fcc79d49",
+    "url": "/static/css/main.172c7f83.chunk.css"
+  },
+  {
+    "revision": "e3a85c2ac8dadb62d2d1",
+    "url": "/static/js/2.0fd91c73.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/static/js/2.0fd91c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "914ee45da1d7fcc79d49",
+    "url": "/static/js/main.ed488924.chunk.js"
+  },
+  {
+    "revision": "3d4965c9977dafc48a8e",
+    "url": "/static/js/runtime-main.fce06d4f.js"
+  },
+  {
+    "revision": "c485a144cb9476f0be1aa7410257780e",
+    "url": "/static/media/Forkinho.c485a144.jpeg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
